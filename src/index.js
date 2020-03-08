7024205375a0bda0fb328a8d2fbee3713ac1e380
dthemg <david.montgomery@live.se>
2020-03-08 22:07:28 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+//import PropTypes from "prop-types"
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class CalcButton extends React.Component {
+    render() {
+        return (
+            <button onClick = { this.props.onClick }>
+                B
+            </button>
+        )
+    }
+}
+
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+
+        this.state = {
+            currVal: 0,
+        }
+    }
+
+    onClick() {
+        this.setState( state => ({
+            currVal: state.currVal + 1,
+        }) )
+        console.log(this.state.currVal);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <CalcButton
+                    onClick = { this.onClick }
+                />
+            </div>
+        )
+
+    }
+
+}
+
+ReactDOM.render(
+    <Calculator/>,
+    document.getElementById("root")
+)
